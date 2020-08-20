3a82cd4b916dac9c5bdeec1133f0153cd0d688e1
masa <masatoshi.sboard@gmail.com>
2020-08-20 23:21:29 +0900

    Done a

--- src/bin/a.rs
+++ src/bin/a.rs
@@ -4,5 +4,15 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n:usize,
+        mut bottuns: [(usize, usize); n],
     }
+    let mut ans = 0;
+    for i in 0..n {
+        let amari = (bottuns[n - 1 - i].0 + ans) % bottuns[n - 1 - i].1;
+        if amari != 0 {
+            ans = ans + bottuns[n - 1 - i].1 - amari;
+        }
+    }
+    println!("{}", ans);
 }
